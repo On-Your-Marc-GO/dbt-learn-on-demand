3e198a1b521b6927ade586429abd43fe60763271
Marc Arguijo <marcos.a.arguijo@gmail.com>
2024-05-10 17:15:27 +0000

    Move reusable componet to intermediate model;

--- models/marts/core/fct_customer_orders.sql
+++ models/marts/core/fct_customer_orders.sql
@@ -16,90 +16,59 @@ with customers as (
 
 ),
 
-orders as (
-
-    select * from {{ ref('stg_jaffle_shop__orders') }}
-
-),
-
-payments as (
-
-    select * from {{ ref('stg_stripe__payments') }}
-
-),
-
-completed_payments as (
-    select 
-        order_id, 
-        max(payment_created_at) as payment_finalized_date, 
-        sum(payment_amount)  as total_amount_paid
-    from payments
-    where payment_status <> 'fail'
-    group by 1
-),
-
--- customer_orders as (
---     select 
---         customers.customer_id,
---         min(orders.order_placed_at) as first_order_date,
---         max(orders.order_placed_at) as most_recent_order_date,
---         count(orders.customer_id) as number_of_orders
---     from customers 
---     left join orders on orders.customer_id = customers.customer_id
---     group by 1
--- ),
-
 paid_orders as (
-    select 
-        orders.order_id,
-        orders.customer_id,
-        orders.order_placed_at,
-        orders.order_status,
-        completed_payments.total_amount_paid,
-        completed_payments.payment_finalized_date,
-        customers.customer_first_name,
-        customers.customer_last_name,
-        customers.full_name
-    from orders
-    left join completed_payments on orders.order_id = completed_payments.order_id
-    left join customers on orders.customer_id = customers.customer_id
+
+    select * from {{ ref('int_orders') }}
+
 ),
+
 
 final as (
     select
-        order_id,
-        customer_id,
-        order_placed_at,
-        order_status,
-        total_amount_paid,
-        payment_finalized_date,
-        customer_first_name,
-        customer_last_name,
+        paid_orders.order_id,
+        paid_orders.customer_id,
+        paid_orders.order_placed_at,
+        paid_orders.order_status,
+        paid_orders.total_amount_paid,
+        paid_orders.payment_finalized_date,
+        customers.customer_first_name,
+        customers.customer_last_name,
+
         -- Sales Transaction Sequence
-        row_number() over (order by order_id) as transaction_seq,
+        row_number() over (
+            order by paid_orders.order_placed_at, paid_orders.order_id
+            ) as transaction_seq,
+
         -- Customer Sales Sequence
-        row_number() over (partition by customer_id order by order_id) as customer_sales_seq,
+        row_number() over (
+            partition by paid_orders.customer_id 
+            order by paid_orders.order_placed_at, paid_orders.order_id
+            ) as customer_sales_seq,
+
         -- New vs Returning Customer
         case 
             when (
             rank() over (
-                partition by customer_id
-                order by order_placed_at, order_id
+                partition by paid_orders.customer_id
+                order by paid_orders.order_placed_at, paid_orders.order_id
                 ) = 1
             ) then 'new'
         else 'return' end as nvsr,
+
         -- Customer Lifetime Value calc
-        sum(total_amount_paid) over (
-            partition by customer_id
-            order by order_placed_at
+        sum(paid_orders.total_amount_paid) over (
+            partition by paid_orders.customer_id
+            order by paid_orders.order_placed_at, paid_orders.order_id
         ) as customer_lifetime_value,
+
+
         -- First Day of Sale
-        first_value(order_placed_at) over (
-            partition by customer_id
-            order by order_placed_at
+        first_value(paid_orders.order_placed_at) over (
+            partition by paid_orders.customer_id
+            order by paid_orders.order_placed_at, paid_orders.order_id
         ) as fdos
     from paid_orders
+    left join customers on paid_orders.customer_id = customers.customer_id
 )
 
 select * from final
-order by order_id
